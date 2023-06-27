6a546cf5a0604f5ba5ae417ee9a24ea0f7da67b9
Jorge M Guimaraes <jmguimaraes@id.uff.br>
2023-06-26 22:38:35 -0300

    fix: split thml to js and css

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var greetingElement = document.getElementById('greeting');
+    greetingElement.innerHTML = 'Hello, OpenAI!'; // Modify the greeting text
+  });
